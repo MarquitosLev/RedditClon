8d69f9d83ec27c12e086cda6d4d14ca436c6879b
Marquitos Leiva <marcleiva623@gmail.com>
2025-10-27 02:43:34 +0000

    TimeZone | FIX Correción al problema del timezone al iniciar el backend

--- RedditClon-Backend/src/main/java/com/RedditClon_Backend/RedditClon_Backend/RedditClonBackendApplication.java
+++ RedditClon-Backend/src/main/java/com/RedditClon_Backend/RedditClon_Backend/RedditClonBackendApplication.java
@@ -3,11 +3,15 @@ package com.RedditClon_Backend.RedditClon_Backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class RedditClonBackendApplication {
 
 	public static void main(String[] args) {
-
+		// Configurar zona horaria UTC para toda la aplicación
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+		
         SpringApplication.run(RedditClonBackendApplication.class, args);
 	}
 
